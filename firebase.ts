40ab242cbc8f24df7e332b62cb903847cc211d3d
Pranav210905 <pranavgudimella09@gmail.com>
2025-05-15 12:51:25 +0530

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -4,13 +4,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBG_2JFuHnQmGEx6WUAYlB1utjwKoyn1Kg",
-  authDomain: "home-d32c2.firebaseapp.com",
-  projectId: "home-d32c2",
-  storageBucket: "home-d32c2.appspot.com",
-  messagingSenderId: "657371589214",
-  appId: "1:657371589214:web:07f2bf68c411ad0b9be2cf",
-  measurementId: "G-F3QRBEM9TW"
+  apiKey: "AIzaSyDV8rJR3q28K7ZkHcbcfjXLZafFqc8vu-I",
+  authDomain: "home-service-b16c5.firebaseapp.com",
+  projectId: "home-service-b16c5",
+  storageBucket: "home-service-b16c5.firebasestorage.app",
+  messagingSenderId: "684934498368",
+  appId: "1:684934498368:web:09a364961718d80282f7a5"
 };
 
 const app = initializeApp(firebaseConfig);
